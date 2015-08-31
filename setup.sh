f941f91da4b00681078d165fbdb5a4659682451e
Rafael Gonzalez <astrorafael@yahoo.es>
2015-08-31 15:42:41 +0200

    cleaned up start.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # EMA setup script
 
 # Only needed for standalone deployment
@@ -7,60 +7,81 @@
 # Preresuisites python2.7 and setuptools
 
 echo
-echo "Installing EMA Package..."
-echo
+echo "------------------------"
+echo "Installing EMADB Package"
+echo "------------------------"
 
 python setup.py install
 
+# ----------------------------
+# Copy scripts
+# can be overriden every time
+# just like the python modules
+# ----------------------------
 
-# Add config file if it does not exist
-if [ ! -f "/etc/ema/config" ]; then
-	echo "Copying emad config file..."
-	mkdir /etc/ema 2>/dev/null 1>/dev/null
-fi
-cp -vf config /etc/ema/config
+echo
+echo "---------------"
+echo "Copying scripts"
+echo "---------------"
 
-# Add defaults file if it does not exist
-if [ ! -f "/etc/default/emad" ]; then
-	echo "Copying defaults for emad service script ..."
-	cp -vf default /etc/default/emad
-fi
+# daemon in foreground utility
+cp -vf scripts/emadb.sh  /usr/local/bin/ema
+chmod 0755 /usr/local/bin/ema
 
-# Add service/daemon script
-if [ ! -f "/etc/init.d/emad" ]; then
-	echo "Installing startup script..."
-	cp -vf emad.init.sh /etc/init.d/emad
-	chmod 0755 /etc/init.d/emad
-fi
-echo "Installing startup script..."
+# init.d service script
 cp -vf emad.init.sh /etc/init.d/emad
 chmod 0755 /etc/init.d/emad
-
-# Add ema command line utility
-echo "Installing EMA daemon as foreground ..."
-cp -rf emad.sh /usr/local/bin/emad
-chmod 0755 /usr/local/bin/emad
-
-
-# Add logrotate handling
-echo "Installing logrotate handling ..."
-cp -rf emad.logrotate /etc/logrotate.d/emad
-
-
 
 # Add auxiliar event scripts and command line utility
 for script in ema low-volt-sms+shutdown roof-script  volt-script 
 do
   echo "Installing auxiliar event script $script ..."
-  cp -rf scripts/$script /usr/local/bin/$script
+  cp -vf scripts/$script /usr/local/bin/$script
   chmod 0755 /usr/local/bin/$script
 done
 
-# Display EMA usages
+
+# --------------------------------------------------
+# Copy config files only once
+# (be polite with with your existing configurations)
+# --------------------------------------------------
+
 echo
-echo "EMA successfully installed"
-echo "* To start EMA daemon in the foreground\t: sudo emad [-h]"
+echo "-------------------------"
+echo "Copying emad config files"
+echo "-------------------------"
+
+# python config file
+
+if [ ! -d "/etc/emad" ]; then
+    echo "creating /etc/emad as the default config directory"
+    mkdir /etc/emad 2>/dev/null 1>/dev/null
+fi
+
+for file in config
+do
+    if [ ! -f "/etc/emadb/$file" ]; then
+	cp -vf config/$file /etc/emadb/
+	chmod 0644 /etc/emadb/$file
+    else
+	echo "skipping /etc/emad/$file"
+    fi
+done
+
+# service defaults file
+
+if [ ! -f "/etc/default/emad" ]; then
+    cp -vf default /etc/default/emad
+else
+    echo "skipping /etc/default/emad"
+fi
+
 echo
-echo "* To start EMA Daemon background\t: sudo service emad start"
-echo "* To start EMA Daemon at boot\t: sudo update-rc.d emad defaults"
+echo "=================================================================="
+echo "EMAD successfully installed"
+echo "* To start EMAD daemon in the foreground\t: sudo emad"
+echo
+echo "* To start EMAD Daemon background\t: sudo service emad start"
+echo "* To start EMAD Daemon at boot\t: sudo update-rc.d emad defaults"
+echo "=================================================================="
 
