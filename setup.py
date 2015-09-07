d95cc55672616a267857df9f319915c390140238
Rafael Gonzalez <astrorafael@yahoo.es>
2015-09-07 23:23:58 +0200

    added versioneer support

--- setup.py
+++ setup.py
@@ -1,19 +1,22 @@
 from setuptools import setup, Extension
+import versioneer
 
-classifiers = ['Development Status :: 3 - Alpha',
-               'Operating System :: POSIX :: Linux',
-               'License :: OSI Approved :: MIT License',
-               'Intended Audience :: Developers',
-               'Programming Language :: Python :: 2.7',
-               'Operating System :: POSIX',
-               'Topic :: Astronomy',
-               'Topic :: Meteorology',
-               'Topic :: Raspberry Pi',
-               'Development Status :: 4 - Beta',
-               ]
+classifiers = [
+    'Environment :: No Input/Output (Daemon)',
+    'Intended Audience :: Science/Research',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: POSIX :: Linux',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Scientific/Engineering :: Astronomy'
+'    Topic :: Scientific/Engineering :: Atmospheric Science'
+    'Development Status :: 4 - Beta',
+]
 
-setup(name             = 'EMA',
-      version          = '0.1.0',
+
+setup(name             = 'ema',
+      version          = versioneer.get_version(),
+      cmdclass         = versioneer.get_cmdclass(),
       author           = 'Rafael Gonzalez',
       author_email     = 'astrorafael@yahoo.es',
       description      = 'A package to monitor & control EMA Weather Station',
