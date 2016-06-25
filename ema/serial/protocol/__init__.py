ec988698abc560534fc608f50829ff25ec4c2cae
Rafael Gonzalez <astrorafael@yahoo.es>
2016-06-25 13:32:21 +0200

    working on voltmeter

--- ema/serial/protocol/__init__.py
+++ ema/serial/protocol/__init__.py
@@ -20,7 +20,25 @@ from __future__ import division
 
 from .base   import EMAProtocol,   EMAProtocolFactory
 from .error  import EMARangeError, EMAReturnError, EMATimeoutError
-from .status import PERIOD
+from .status import (
+	PERIOD, ROOF_RELAY,
+	AUX_RELAY,
+	POWER_VOLT,
+	RAIN_PROB,
+	CLOUD_LEVEL,
+	ABS_PRESSURE,
+	CAL_PRESSURE,
+	CUR_PLUVIOM,
+	ACC_PLUVIOM,
+	PYRANOMETER,
+	PHOTOM_FREQ,
+	AMB_TEMP,
+	HUMIDITY,
+	DEW_POINT,
+	CUR_WIND_SPEED,
+	AVE_WIND_SPEED,
+	WIND_DIRECTION,
+)
 
 # ----------------
 # Module constants
@@ -37,5 +55,22 @@ __all__ = [
 	EMATimeoutError,
 	EMARangeError, 
 	EMAReturnError,
-	PERIOD
+	PERIOD,
+	ROOF_RELAY,
+	AUX_RELAY,
+	POWER_VOLT,
+	RAIN_PROB,
+	CLOUD_LEVEL,
+	ABS_PRESSURE,
+	CAL_PRESSURE,
+	CUR_PLUVIOM,
+	ACC_PLUVIOM,
+	PYRANOMETER,
+	PHOTOM_FREQ,
+	AMB_TEMP,
+	HUMIDITY,
+	DEW_POINT,
+	CUR_WIND_SPEED,
+	AVE_WIND_SPEED,
+	WIND_DIRECTION,
 ]
--- ema/serial/voltmeter.py
+++ ema/serial/voltmeter.py
@@ -36,8 +36,10 @@ from .devices   import Device
 from .protocol  import (
     EMAProtocol, EMAProtocolFactory, 
     EMARangeError, EMAReturnError, EMATimeoutError,
-    PERIOD as EMA_PERIOD
+    PERIOD as EMA_PERIOD, POWER_VOLT
 )
+
+from ..utils import chop
 
 # ----------------
 # Module constants
@@ -80,7 +82,31 @@ class Voltmeter(Device):
                 'set':   self.parent.protocol.setVoltmeterOffset
             },
         }
-        self.values = deque(maxlen=(upload_period//EMA_PERIOD))
+        self.voltage = deque(maxlen=(upload_period//EMA_PERIOD))
+        self.parent.protocol.addStatusCallback(self.onStatus)
+        scripts = chop(options["script"], ',')
+        for script in scripts:
+            pass
+            #self.parent.notifier.addScript('VoltageLow', options['mode'], script)
+
+
+    def onStatus(self, message, timestamp):
+        self.voltage.append(message[POWER_VOLT])
+        accum = sum(self.voltage)
+        n     = len(self.voltage)
+        average = accum / n
+        if  self.PARAMS['threshold'] is None:
+            log.debug("No thershold value yet from EMA")
+            return
+        lowvolt = self.options['delta'] + self.PARAMS['threshold']
+        if average < lowvolt:
+            pass
+            log.warn("Low voltage detected")
+            #self.parent.notifier.onEventExecute('VoltageLow', 
+            #    '--voltage', "{0:.1f}".format(average), 
+            #    '--threshold', "{0:.1f}".format(lowvolt), 
+            #    '--size' , str(n))
+
 
 
 # --------------------------------------------------------------------
