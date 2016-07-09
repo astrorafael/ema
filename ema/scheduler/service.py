b0bbf34d3c46849325403148a4ea3a7432cfd79f
Rafael Gonzalez <astrorafael@yahoo.es>
2016-07-09 08:32:44 +0200

    debugging scheduler

--- ema/scheduler/service.py
+++ ema/scheduler/service.py
@@ -18,7 +18,7 @@ import datetime
 
 from twisted.logger               import Logger, LogLevel
 from twisted.internet             import reactor, task
-from twisted.internet.defer       import inlineCallbacks
+from twisted.internet.defer       import inlineCallbacks, returnValue
 #from twisted.application.service  import Service
 
 #--------------
@@ -41,6 +41,10 @@ from .error    import BadSlice
 # Global functions
 # -----------------
 
+def toRefDate(ts):
+    '''
+    Sets the datetome.datetime object ts to the given reference date'''
+    return ts.replace(year=2000, month=1, day=1, microsecond=0)
 
 # -----------------------
 # Module global variables
@@ -83,15 +87,19 @@ class SchedulerService(Service):
         Service.stopService(self)
 
 
-    def addActivity(self, func, sliceperc):
+    def addActivity(self, func, sliceperc, active, inactive):
         '''
-        Add and activity (a callable) to be called at the current or next active window.
+        Add and activity (a callable) to be called active window.
         '''
         if sliceperc not in [10, 30, 50, 70, 90]:
             raise BadSlice(sliceperc)
-        active, inactive, where = self.findCurrentInterval()
         tPerc = (active.t0 + datetime.timedelta(seconds=int(active.duration()*sliceperc/100))).time()
-        log.debug("Interval = {interval}, tPerc = {tPerc}", interval=active, tPerc=tPerc)
+        log.debug("Adding activity to interval = {interval}, tPerc = {tPerc}", interval=active, tPerc=tPerc)
+        values = self.callables.get(tPerc, list())
+        self.callables[tPerc] = values
+        # register non duplicate activities in order
+        if (func, active, inactive) not in values:
+            self.callables[tPerc].append((func, active, inactive))
        
 
     #---------------------
@@ -110,32 +118,60 @@ class SchedulerService(Service):
     # Helper methods
     # ---------------
 
+    @inlineCallbacks
     def _schedule(self):
         '''
         Runs a schedule cycle.
         '''
-        ts = datetime.datetime.utcnow().replace(microsecond=0)
+        now = toRefDate(datetime.datetime.utcnow())
+        tstamps = [ datetime.datetime.combine(now.date(), t) for t in sorted(self.callables.keys(), reverse=False) ]
+        if len(tstamps) == 0:
+            log.debug("Registering all activities again")
+            self.parent.addActivities()
+            returnValue(None)
+
+        for target in tstamps:
+            delta = (target - now).total_seconds()
+            log.debug("target = {target}, delta={delta}", target=target, delta=delta)
+            if 0<= delta < self.T:
+                yield task.deferLater(reactor, delta, lambda: None)
+                info = self.callables[target.time()]
+                del self.callables[target.time()]
+                for item in info: 
+                    item[0](item[1], item[2])
+                break
+            elif delta < 0 and self.findActiveInactive(now) == self.ACTIVE:
+                log.debug("deleting OBSOLETE activity at {target}", target=target)
+                info = self.callables[target.time()]
+                del self.callables[target.time()]
+               
 
     def findCurrentInterval(self):
         '''Find the current interval'''     
         tNow = datetime.datetime.utcnow()
-        log.debug("checking active intervals {active}", active=self.windows)
         found, i = self.windows.find(tNow)
         if found:
-            where    = self.ACTIVE
             active   = self.windows[i]
             inactive = self.gaps[i]
-            log.info("now {now} we are in the active window {window}", now=tNow.strftime("%H:%M:%S"), window=active)
-            log.info("Next inactive gap will be {gap}",  gap=inactive)
+            log.debug("now {now} we are in the active window {window}", now=tNow.strftime("%H:%M:%S"), window=active)
+            log.debug("Next inactive gap will be {gap}",  gap=inactive)
         else:
-            log.debug("checking inactive intervals {inactive}", inactive=self.gaps)
-            where = self.INACTIVE
             found, i = self.gaps.find(tNow)
             inactive = self.gaps[i]
             active   = self.windows[i+1 % len(self.windows)]
-            log.info("now {now} we are in the inactive window {gap}", now=tNow.strftime("%H:%M:%S"), gap=inactive)
-            log.info("Next active gap will be {window}", window=active)
-        return active, inactive, where
+            log.debug("now {now} we are in the inactive window {gap}", now=tNow.strftime("%H:%M:%S"), gap=inactive)
+            log.debug("Next active gap will be {window}", window=active)
+        return active, inactive
+
+
+    def findActiveInactive(self, tNow):
+        '''A shorter version which only returns ACTIVE or INACTIVE state'''
+        found, i = self.windows.find(tNow)
+        if found:
+            where = self.ACTIVE
+        else:
+            where = self.INACTIVE
+        return where
            
 __all__ = [
     "ScheduleService"
